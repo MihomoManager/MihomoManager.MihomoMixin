08c5f947750b46f8211e76fe6b59f2c3d809bf39
yueyinqiu <yueyinqiu@outlook.com>
2026-09-20 20:37:52 +0800

    publish

--- flake.nix
+++ flake.nix
@@ -26,6 +26,13 @@
                     -o "src/MihomoManager.MihomoMixin/Js/js-yaml.umd.min.js"
                 '';
               })
+              (pkgs.writeShellApplication {
+                name = "dev-publish";
+                text = ''
+                  mkdir -p publish
+                  dotnet pack src/MihomoManager.MihomoMixin/MihomoManager.MihomoMixin.csproj -c Release -o publish
+                '';
+              })
             ];
             shellHook = ''
               export DOTNET_ROOT="${pkgs.dotnetCorePackages.sdk_10_0}/share/dotnet"
