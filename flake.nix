a0819896c8f5561f091e4aa9e105aad744c95915
yueyinqiu <yueyinqiu@outlook.com>
2026-09-20 20:06:44 +0800

    dl

--- flake.nix
+++ flake.nix
@@ -18,6 +18,14 @@
           default = pkgs.mkShell {
             packages = [
               pkgs.dotnetCorePackages.sdk_10_0
+              (pkgs.writeShellApplication {
+                name = "dev-update-js-yaml";
+                text = ''
+                  "${pkgs.curl}/bin/curl" -fsSL \
+                    "https://cdn.jsdelivr.net/npm/js-yaml/dist/browser/js-yaml.umd.min.js" \
+                    -o "src/MihomoManager.MihomoMixin/Js/js-yaml.umd.min.js"
+                '';
+              })
             ];
             shellHook = ''
               export DOTNET_ROOT="${pkgs.dotnetCorePackages.sdk_10_0}/share/dotnet"
